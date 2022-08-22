2d3ab21b7f0fd753f5922d9eb2610b4c9c4b5f13
Jason Bell <jbell@accelebrate.com>
2022-08-22 10:03:58 -0700

    Print board

---
+++ TicTacToe/TicTacToe.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToe.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine("   | X |   ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine("   |   | O ");
+        }
+    }
+}
