a7da769ecd7e30e6ca11e5bbc83b923eb0925dd2
Jason Bell <jbell@accelebrate.com>
2022-08-23 12:37:49 -0700

    LINQ

---
+++ LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+var nums = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+var evens = nums.Where(n => n % 2 == 0);
+
+foreach (var num in evens) {
+    Console.WriteLine(num);
+}
+
+Console.WriteLine("=================");
+
+nums.Add(8);
+
+foreach (var num in evens) {
+    Console.WriteLine(num);
+}
+
