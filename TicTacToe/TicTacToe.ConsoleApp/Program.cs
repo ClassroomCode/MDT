23175265b82c69ad5979c35bc5c59de2d25f1d5a
Jason Bell <jbell@accelebrate.com>
2022-08-23 08:36:09 -0700

    Lab Start

--- TicTacToe/TicTacToe.ConsoleApp/Program.cs
+++ TicTacToe/TicTacToe.ConsoleApp/Program.cs
@@ -6,7 +6,25 @@ namespace TicTacToe.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var b = new Board();
+            var g = new Game();
+            DrawBoard(g.Board);
+
+            Console.Write("Where do you want to move (1-9)? ");
+            var input = Console.ReadLine();
+
+            int move;
+            if (!int.TryParse(input, out move)) {
+                // TODO: bad, ask again
+            }
+            g.MakeMove(move);   // TODO: returns true of false
+            DrawBoard(g.Board);
+
+            // TODO: Next player
+        }
+
+        static void DrawBoard(Board b)
+        {
+            // TODO: do it right
 
             Console.WriteLine("   |   |   ");
             Console.WriteLine("---+---+---");
---
+++ TicTacToe/TicTacToe.Lib/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Lib
+{
+    public class Game
+    {
+        public Piece CurrentPlayer { get; } = Piece.X;
+
+        public Board Board { get; } = new Board();
+
+        public bool MakeMove(int pos)
+        {
+            // TODO: implement
+
+            return true;
+        }
+    }
+}
