d502abef326aee20e1f66671b2da1f7cd3f31b4c
Jason Bell <jbell@accelebrate.com>
2022-08-23 09:29:07 -0700

    Working (full)

--- TicTacToe/TicTacToe.ConsoleApp/Program.cs
+++ TicTacToe/TicTacToe.ConsoleApp/Program.cs
@@ -9,28 +9,54 @@ namespace TicTacToe.ConsoleApp
             var g = new Game();
             DrawBoard(g.Board);
 
+            while (!g.Board.IsFull) {
+                int? m = null;
+                while (!m.HasValue) {
+                    m = AskForMove();
+                    if (!m.HasValue) {
+                        Console.WriteLine("INVALID MOVE");
+                    }
+                    else {
+                        bool b = g.MakeMove(m.Value);
+                        if (!b) {
+                            Console.WriteLine("POSITION TAKEN");
+                            m = null;
+                        }
+                    }
+                }
+                DrawBoard(g.Board);
+            }
+        }
+
+        static int? AskForMove()
+        {
             Console.Write("Where do you want to move (1-9)? ");
             var input = Console.ReadLine();
 
             int move;
             if (!int.TryParse(input, out move)) {
-                // TODO: bad, ask again
+                return null;
             }
-            g.MakeMove(move);   // TODO: returns true of false
-            DrawBoard(g.Board);
-
-            // TODO: Next player
+            if (move < 1 || move > 9) return null; 
+            return move;
         }
 
         static void DrawBoard(Board b)
         {
-            // TODO: do it right
+            Console.WriteLine(StringForRow(b[1], b[2], b[3]));
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(StringForRow(b[4], b[5], b[6]));
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(StringForRow(b[7], b[8], b[9]));
+        }
 
-            Console.WriteLine("   |   |   ");
-            Console.WriteLine("---+---+---");
-            Console.WriteLine("   | X |   ");
-            Console.WriteLine("---+---+---");
-            Console.WriteLine("   |   | O ");
+        static string StringForRow(Piece p1, Piece p2, Piece p3) =>
+            $" {StringForPiece(p1)} | {StringForPiece(p2)} | {StringForPiece(p3)} ";
+
+        static string StringForPiece(Piece p)
+        {
+            if (p == Piece.Empty) return " ";
+            return (p == Piece.X ? "X" : "O");
         }
     }
 }
--- TicTacToe/TicTacToe.Lib/Game.cs
+++ TicTacToe/TicTacToe.Lib/Game.cs
@@ -6,14 +6,17 @@ namespace TicTacToe.Lib
 {
     public class Game
     {
-        public Piece CurrentPlayer { get; } = Piece.X;
+        public Piece CurrentPlayer { get; private set; } = Piece.X;
 
         public Board Board { get; } = new Board();
 
         public bool MakeMove(int pos)
         {
-            // TODO: implement
+            if (Board[pos] != Piece.Empty) return false;
 
+            Board[pos] = CurrentPlayer;
+
+            CurrentPlayer = (CurrentPlayer == Piece.X ? Piece.O : Piece.X);
             return true;
         }
     }
